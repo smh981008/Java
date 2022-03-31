fb61801accd0a52aee6cf2e16cb67724a4e0037a
smh981008 <44371533+smh981008@users.noreply.github.com>
2022-03-31 09:39:38 +0800

    1

--- src/Solution.java
+++ src/Solution.java
@@ -2,3 +2,10 @@ import javax.swing.tree.TreeNode;
 import java.net.Inet4Address;
 import java.util.*;
 import java.util.stream.Collectors;
+
+class Solution {
+    public String nearestPalindromic(String n) {
+        int length = n.length();
+        
+    }
+}
